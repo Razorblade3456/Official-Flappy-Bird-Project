84b355508562eedac5d77d894251d11f55b975d6
Razorblade3456 <abraham1leyva@gmail.com>
2025-04-11 13:44:09 -0700

    Added moving pipes

---
+++ flappybird.js
@@ -0,0 +1,92 @@
+//board
+let board;
+let boardWidth = 360;
+let boardHeight = 640;
+let context;
+
+//bird
+let birdWidth = 34; //width/height ratio = 408/228 = 17/12
+let birdHeight = 24;
+let birdX = boardWidth/8;
+let birdY = boardHeight/2;
+let birdImg;
+
+let bird = {
+    x : birdX,
+    y : birdY,
+    width : birdWidth,
+    height : birdHeight
+}
+
+//pipes
+let pipeArray = [];
+let pipeWidth = 64; //width and height ratio = 384/3072 = 1/8
+let pipeHeight = 512;
+let pipeX = boardWidth;
+let pipeY = 0;
+
+let topPipeImg;
+let bottomPipeImg;
+
+//game physics
+let velocityX = -2; //This is the pipes moving to the left
+
+window.onload = function () {
+    board = document.getElementById("board");
+    board.height = boardHeight;
+    board.width = boardWidth;
+    context = board.getContext("2d"); //used for drawing on the board
+
+    //drawing the bird
+    //context.fillStyle = "green";
+    //context.fillRect(bird.x, bird.y, bird.width, bird.height);
+
+    //loading images
+    birdImg = new Image();
+    birdImg.src = "./BMO2.png"
+    birdImg.onload = function() {
+        context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+    }
+
+    topPipeImg = new Image();
+    topPipeImg.src = "./toppipe.png";
+
+    bottomPipeImg = new Image();
+    bottomPipeImg.src = "./bottompipe.png";
+
+    requestAnimationFrame(update);
+    setInterval(placePipes, 1500); //the set interval is every 1.5 seconds because 1500 miliseconds is 1.5 seconds
+
+}
+
+function update() {
+    requestAnimationFrame(update);
+    context.clearRect(0, 0, board.width, board.height);
+
+    //bird
+    context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+
+    //pipes
+    for (let i = 0; i < pipeArray.length; i ++) {
+        let pipe = pipeArray[i];
+        pipe.x += velocityX;
+        context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height);
+    }
+ }
+
+function placePipes() {
+
+    //y starts at 0, 512/4=128 0-128= -128px for the pipe placement
+    let randompipeY = pipeY - pipeHeight/4 - Math.random()*(pipeHeight/2); 
+
+    let topPipe = {
+        img : topPipeImg,
+        x : pipeX,
+        y : randompipeY,
+        width : pipeWidth,
+        height : pipeHeight,
+        passed : false
+    }
+
+    pipeArray.push(topPipe);
+}
