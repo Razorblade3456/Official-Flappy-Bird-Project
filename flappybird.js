6af22001c0c989858e08d576be49cb47ae46b1fb
Razorblade3456 <abraham1leyva@gmail.com>
2025-04-11 20:44:02 -0700

    Official Flappy Bird Project

--- flappybird.js
+++ flappybird.js
@@ -30,6 +30,11 @@ let bottomPipeImg;
 
 //game physics
 let velocityX = -2; //This is the pipes moving to the left
+let velocityY = 0; //bird jump speed
+let gravity = 0.4;
+
+let gameOver = false
+let score = 0;
 
 window.onload = function () {
     board = document.getElementById("board");
@@ -56,25 +61,64 @@ window.onload = function () {
 
     requestAnimationFrame(update);
     setInterval(placePipes, 1500); //the set interval is every 1.5 seconds because 1500 miliseconds is 1.5 seconds
+    document.addEventListener("keydown", moveBird);
 
 }
 
 function update() {
     requestAnimationFrame(update);
+    if (gameOver) {
+        return;
+    }
     context.clearRect(0, 0, board.width, board.height);
 
     //bird
+    velocityY += gravity;
     context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+    bird.y = Math.max(bird.y + velocityY, 0); //apply gravity to current bird.y to limit the bird.y to top of the canvas
+
+    if(bird.y > board.height) {
+        gameOver = true;
+    }
 
     //pipes
     for (let i = 0; i < pipeArray.length; i ++) {
         let pipe = pipeArray[i];
         pipe.x += velocityX;
         context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height);
+
+        if (!pipe.passed && bird.x > pipe.x + pipe.width) {
+            score += 0.5; //0.5 because there are 2 pipes! 0.5*2=1, so it is 0.5 to equal 1
+            pipe.passed = true;
+        }
+
+        if (detectCollision(bird, pipe)) {
+            gameOver = true;
+        }
+    }
+
+    //clearing the pipes
+    while (pipeArray.length > 0 && pipeArray[0].x < -pipeWidth) {
+        pipeArray.shift(); //removes first element from array
+    }
+
+
+    //score 
+    context.fillStyle = "aqua";
+    context.font = "45px sans-serif";
+    context.fillText(score, 5, 45);
+
+    if (gameOver) {
+        context.fillText("GAME OVER", 5, 90);
+        context.fillText("Tap space to try", 18, 250);
+        context.fillText("again", 125, 300);
     }
  }
 
 function placePipes() {
+    if (gameOver) {
+        return;
+    } 
 
     //y starts at 0, 512/4=128 0-128= -128px for the pipe placement
     let randompipeY = pipeY - pipeHeight/4 - Math.random()*(pipeHeight/2); 
@@ -101,4 +145,26 @@ function placePipes() {
         passed : false
     }
     pipeArray.push(bottomPipe);
+}
+
+function moveBird(e) {
+    if (e.code == "Space" || e.code == "ArrowUp" || e.code == "keyX") {
+        //jump
+        velocityY = -6;
+
+        //resetting the game
+        if(gameOver) {
+            bird.y = birdY;
+            pipeArray = [];
+            score = 0;
+            gameOver = false;
+         }
+    }
+}
+
+function detectCollision(a, b) {
+    return a.x < b.x + b.width &&
+            a.x + a.width > b.x &&
+            a.y < b.y + b.height &&
+            a.y + a.height> b.y;
 }
