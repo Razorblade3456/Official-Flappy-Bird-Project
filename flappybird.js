3ca88e44bb340fb021b20594ce311ed8bce1813d
Razorblade3456 <abraham1leyva@gmail.com>
2025-04-11 20:57:55 -0700

    Changed the gravity back to normal

--- flappybird.js
+++ flappybird.js
@@ -31,7 +31,7 @@ let bottomPipeImg;
 //game physics
 let velocityX = -2; //This is the pipes moving to the left
 let velocityY = 0; //bird jump speed
-let gravity = 0.1;
+let gravity = 0.4;
 
 let gameOver = false
 let score = 0;
